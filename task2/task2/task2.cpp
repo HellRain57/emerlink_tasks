e2a9d030f1f27b1519a7b1df216c0114c8b3a32e
HellRain57 <109213391+HellRain57@users.noreply.github.com>
2023-07-11 17:38:51 +0300

    Add 1,2 and 5 tasks

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+class a1 {
+private:
+	int* data = NULL;
+public:
+	~a1() { Stop(); };
+
+	virtual void Start() {
+		if (data)
+			delete[] data;
+
+		data = new int[10000];
+	}
+
+	virtual void Stop() {
+		if (data) {
+			delete[] data;
+			data = NULL;
+		}
+	}
+};
+
+class a2 : public a1 {
+private:
+	int* data2 = NULL;
+
+public:
+
+	void Start() {
+		a1::Start();
+
+		if (data2)
+			delete[] data2;
+
+		data2 = new int[10000];
+		Stop(); // ну технически оно работает🌚
+	}
+
+	void Stop() override {
+		a1::Stop();
+
+		if (data2) {
+			delete[] data2;
+			data2 = NULL;
+		}
+	}
+};
+
+void Proc() {
+
+	a1* A = new a2();
+	A->Start();
+	delete A;
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	for (;;) Proc();
+
+}
---
+++ task5/task5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO;
+using System.Text;
+
+record class ParamsForThreads(int startNum, int endNum);
+
+class Program
+{
+    static FileStream fstream = new FileStream("primes.txt", FileMode.Create);
+    static object locker = new();
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите Nt: ");
+        int Nt = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите IMax: ");
+        int IMax = Convert.ToInt32(Console.ReadLine());
+
+        //int Nt = 7;
+        //int IMax = 1011;
+
+
+        double iterForThread = (double)IMax / (double)Nt; // считаем сколько приходится на каждый поток
+        int extraIter = IMax - (Nt * (int)iterForThread); // порой числа между потоками поровну не делится. считаем сколько потоков остается
+
+        int start = 0;
+        int stop = 0;
+
+        fstream.Close(); //перед запуском, файл пересоздается и открывается. Вот здесь он закрывается
+
+        List<Thread> threads = new List<Thread>();
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        for (int i = 0; i < Nt; i++)
+        {
+            start = stop;
+            stop = start + (int)iterForThread;
+            if (extraIter > 0)
+            {
+                stop++; // если есть числа, которые по ровну между потоками не делятся, то раскидываем по одному числу каждому потоку начиная с первого
+                extraIter--;
+            }
+
+            ParamsForThreads paramsForThreads = new ParamsForThreads(start, stop); // указываем каждому потоку, с какого числа по какое проверять
+            Thread myThread = new(checkPrimeNums);
+            threads.Add(myThread);
+            threads[i].Name = $"Поток {i}";
+            threads[i].Start(paramsForThreads);
+        }
+        for (int i = 0; i < Nt; i++)
+        {
+            threads[i].Join();
+        }
+        //останавливаем счётчик
+        stopwatch.Stop();
+        //смотрим сколько тактов было затрачено на выполнение
+        Console.WriteLine("Время выполнения: " + stopwatch.ElapsedMilliseconds + " мс");
+    }
+
+    static bool isPrime(int n)
+    {
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                //n не простое
+                return false;
+            }
+        }
+        // n простое.
+        return true;
+    }
+
+    static void checkPrimeNums(object? obj)
+    {
+        string text = "";
+        if (obj is ParamsForThreads paramsForThreads)
+        {
+            for (int i = paramsForThreads.startNum; i < paramsForThreads.endNum; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + ": " + i);
+                    text += Thread.CurrentThread.Name + ": " + i + "| "; // строка для записи
+                }
+            }
+            text += "\n";
+            // преобразуем строку в байты
+            byte[] buffer = Encoding.Default.GetBytes(text);
+            lock (locker)
+            {
+                fstream = new FileStream("primes.txt", FileMode.Append);
+
+                fstream.Write(buffer, 0, buffer.Length);
+                fstream.Close();
+                Console.WriteLine("Текст записан в файл ==== " + Thread.CurrentThread.Name);
+            }
+
+        }
+    }
+
+}
